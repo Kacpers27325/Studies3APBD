77b605bccccbbf08550e0d048cffb5ec65c76938
kacpers27325 <s27325@pjwstk.edu.pl>
2024-03-15 12:25:31 +0100

    Podstawy1

--- .idea/Kontener.cs
+++ .idea/Kontener.cs
@@ -2,5 +2,12 @@
 
 public class Kontener
 {
+    private int masa;
+    private int wysokosc;
+    private int waga_wlasna;
+    private int glebokosc;
+    private string numer_seryjny;
+    private int max_ladownosc;
     
+
 }
