ba630f5fadb497efdcbe9f35c1612db309b26856
kacpers27325 <s27325@pjwstk.edu.pl>
2024-03-15 12:29:56 +0100

    Konstruktor klasy Kontener

--- .idea/Kontener.cs
+++ .idea/Kontener.cs
@@ -8,6 +8,15 @@ public class Kontener
     private int glebokosc;
     private string numer_seryjny;
     private int max_ladownosc;
-    
 
+    public Kontener(int masa, int wysokosc, int waga_wlasna, int glebokosc, string nume_seryjny, int max_ladownosc)
+    {
+        masa = masa;
+        wysokosc = wysokosc;
+        waga_wlasna = waga_wlasna;
+        glebokosc = glebokosc;
+        nume_seryjny = nume_seryjny;
+        max_ladownosc = max_ladownosc;
+    }
+    
 }
