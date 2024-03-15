3c233ca5cc5ae78dc84a2b9205cdfca782740103
kacpers27325 <s27325@pjwstk.edu.pl>
2024-03-15 13:40:25 +0100

    Praca nad CoolingContainer

--- ConsoleApp1/ConsoleApp1/Kontenery/CoolingContainer.cs
+++ ConsoleApp1/ConsoleApp1/Kontenery/CoolingContainer.cs
@@ -1,6 +1,38 @@
-﻿namespace DefaultNamespace;
+﻿using ConsoleApp1;
 
-public class CoolingContainer
+namespace DefaultNamespace;
+
+public class CoolingContainer : Kontener, IHazardnotifier
 {
-    
+    public double temp;
+    public string kind;
+    public CoolingContainer(double temp, string kind, int masa_ladunku, int wysokosc, int waga_wlasna, int glebokosc, string nume_seryjny, double max_ladownosc, bool niebezpieczny) : base(masa_ladunku, wysokosc, waga_wlasna, glebokosc, nume_seryjny, max_ladownosc, niebezpieczny)
+    {
+        temp = temp;
+        kind = kind;
+    }
+
+    public void zaladuj(int ile, int temperatura_produktu)
+    {
+        if (temp > temperatura_produktu)
+        {
+            base.zaladuj(ile);    
+        }
+        else
+        {
+            
+        }
+    }
+
+    public override void oproznij()
+    {
+        double mass = Masa_ladunku;
+        base.oproznij();
+        base.zaladuj((int)(mass*0.05));
+    }
+
+    public void notify()
+    {
+        throw new NotImplementedException();
+    }
 }
