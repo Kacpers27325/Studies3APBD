1b7eee8e3cad49ccf97b2a0964407341ec67145f
kacpers27325 <s27325@pjwstk.edu.pl>
2024-03-15 12:36:04 +0100

    Dodanie metody zaladuj

--- .idea/Kontener.cs
+++ .idea/Kontener.cs
@@ -2,21 +2,40 @@
 
 public class Kontener
 {
-    private int masa;
+    private int masa_ladunku;
     private int wysokosc;
     private int waga_wlasna;
     private int glebokosc;
     private string numer_seryjny;
     private int max_ladownosc;
 
-    public Kontener(int masa, int wysokosc, int waga_wlasna, int glebokosc, string nume_seryjny, int max_ladownosc)
+    public Kontener(int masa_ladunku, int wysokosc, int waga_wlasna, int glebokosc, string nume_seryjny, int max_ladownosc)
     {
-        masa = masa;
+        masa_ladunku = masa_ladunku;
         wysokosc = wysokosc;
         waga_wlasna = waga_wlasna;
         glebokosc = glebokosc;
         nume_seryjny = nume_seryjny;
         max_ladownosc = max_ladownosc;
     }
-    
+
+    public void oproznij
+    {
+        masa=0;
+    }
+
+    public void zaladuj(int ile)
+    {
+        if (masa + ile <= max_ladownosc)
+        {
+            masa = masa + ile;
+        }
+        else
+        {
+            throw new Exception()
+            {
+                Console.WriteLine("OverFillException");
+            }
+        }
+    }
 }
